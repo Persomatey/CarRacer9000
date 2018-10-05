41f9de4f677ed2e9d1e2a97793182c9ec385a45e
Dylan <masterdylan87@yahoo.com>
2018-10-05 12:40:54 -0700

    Finish line scripts, and halfway model/script

---
+++ CompSciClubFall2018/Assets/Scripts/Finish LIne copy/FinishLine1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine1 : MonoBehaviour {
+	public GameObject Lap;
+	public GameObject Halfway;
+
+	void OnTriggerEnter(){
+		Lap.SetActive (true);
+		Halfway.SetActive (false);
+	}
+}
