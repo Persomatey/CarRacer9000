280b4975d6a4409717b5b7769359caa6bda88f89
Persomatey <huntergoodin1@hotmail.com>
2018-11-09 13:40:36 -0800

    For Dylan
    Take this

---
+++ CompSciClubFall2018/Assets/Scripts/LapSystem/Finish LIne copy/Halfwaypoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Halfwaypoint : MonoBehaviour
+{
+	public GameObject finishLap;
+	public GameObject halfway;
+    public bool checker = false;
+
+    private void Update()
+    {
+        if(checker == true)
+        {
+            finishLap.SetActive(true);
+            halfway.SetActive(false);
+        }
+        else if(checker == false)
+        {
+            finishLap.SetActive(false);
+            halfway.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        if(checker == false)
+        {
+            checker = true;
+        }
+        else if(checker == true)
+        {
+            checker = false;
+        }
+	}
+}
