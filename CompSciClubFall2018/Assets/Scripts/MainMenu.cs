8855092e04215d002f7ac2105ee75124aba3d84c
Persomatey <huntergoodin1@hotmail.com>
2018-12-01 14:06:24 -0800

    Initial Push
    Has Stephen's stuff in it. Also included both of the current tracks so Jon can get to work.

---
+++ CompSciClubFall2018/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame()
+    {
+        Debug.Log("Play.");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit.");
+        Application.Quit();
+    }
+}
