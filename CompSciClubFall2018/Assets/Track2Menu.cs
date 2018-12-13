471dedcbc48e606b0be3589e4d1cdd145f8aec73
John Yalung <johnearth0111@gmail.com>
2018-12-12 22:06:22 -0800

    An improved game with a working track selection

--- CompSciClubFall2018/Assets/Track2Menu.cs
+++ CompSciClubFall2018/Assets/Track2Menu.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Track2Menu : MonoBehaviour {
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!!");
+        Application.Quit();
+    }
 }
--- CompSciClubFall2018/Assets/TrackMenu.cs
+++ CompSciClubFall2018/Assets/TrackMenu.cs
@@ -9,4 +9,10 @@ public class TrackMenu : MonoBehaviour {
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!!");
+        Application.Quit();
+    }
 }
